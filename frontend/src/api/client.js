0d6c89bb71498883cd6c42679996e8dbbf69cfda
phundum67 <phundum6@gmail.com>
2026-04-21 22:44:53 +0530

    Change API base URL to production server

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL =  "https://phundum67.pythonanywhere.com/api";
 
 async function request(path, options = {}) {
   const response = await fetch(`${API_BASE_URL}${path}`, {
